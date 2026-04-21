6be1f1311000afe015313bba2f47663fc0f85680
agentb-sch <brayan_lim@cwss.moe.edu.sg>
2026-04-21 11:30:06 +0800

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://agentb-sch.github.io/environment_sensor_pr/](https://agentb-sch.github.io/environment_sensor_pr/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/agentb-sch/environment_sensor_pr** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/agentb-sch/environment_sensor_pr** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,115 @@
+function powerI () {
+    torch = 0
+    strip = neopixel.create(DigitalPin.P1, 10, NeoPixelMode.RGB)
+    strip.clear()
+    OLED.init(128, 64)
+}
+// P1> NEOPIXEL
+// 
+// P9-10> RESERVED
+// 
+// P2-3> lIGHT SENSOR
+// 
+// P4> LED
+function data () {
+    OLED.writeString("Pressure")
+    OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_pressure))
+    OLED.writeString("hPa")
+    OLED.newLine()
+    OLED.writeString("Humidity:")
+    OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_humidity))
+    OLED.writeString("%")
+    OLED.newLine()
+    OLED.writeString("Temperature:")
+    OLED.newLine()
+    OLED.writeNum(Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C))
+    OLED.writeString("°C")
+    OLED.newLine()
+    OLED.writeStringNewLine("Dust level:")
+    OLED.writeNum(Environment.ReadDust(DigitalPin.P9, AnalogPin.P10))
+    OLED.writeString("μg/m³")
+    OLED.newLine()
+    OLED.writeString("Light intensity:")
+    OLED.writeNum(Environment.ReadLightIntensity(AnalogPin.P2))
+    OLED.writeString("%")
+    basic.pause(1000)
+    OLED.clear()
+}
+function torchlight () {
+    if (torch == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        Environment.ledBrightness(AnalogPin.P4, true, 100)
+        torch = 1
+    } else {
+        Environment.ledBrightness(AnalogPin.P4, false, 0)
+        basic.showLeds(`
+            # # . . .
+            # # # . .
+            . # # # .
+            . . # # #
+            . . . # #
+            `)
+        torch = 0
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    torchlight()
+})
+function dust () {
+    if (Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) < 100) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        if (Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) >= 101 && Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) < 200) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+        } else {
+            if (Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) >= 201 && Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) < 300) {
+                strip.showColor(neopixel.colors(NeoPixelColors.Red))
+                basic.showLeds(`
+                    . . . . .
+                    . # . # .
+                    . . . . .
+                    . # # # .
+                    # . . . #
+                    `)
+            } else {
+                if (Environment.ReadDust(DigitalPin.P9, AnalogPin.P10) >= 300) {
+                    strip.showColor(neopixel.colors(NeoPixelColors.White))
+                    basic.showLeds(`
+                        . . # . .
+                        . . # . .
+                        . . # . .
+                        . . . . .
+                        . . # . .
+                        `)
+                }
+            }
+        }
+    }
+}
+let strip: neopixel.Strip = null
+let torch = 0
+powerI()
+basic.forever(function () {
+    data()
+    dust()
+})
